624115f2ba3fb59428ecbafa1c28f4b91a91ac49
paulanomusaz <paulanomusaz@gmail.com>
2023-08-21 06:57:31 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,9 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
- *reset_to_98 - Takes a pointer to an int 
- *and updates the valve it points to 98
+ * reset_to_98 - int the value n
+ * and update the value it points to 98
  * @n: int n
- * Return
  */
 void reset_to_98(int *n)
 {
